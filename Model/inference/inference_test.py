08241fac69b06a7fcd60158eff3d499c5baba9b5
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2026-05-25 19:12:00 +0300

    formatting printed outputs

--- Model/inference/inference_test.py
+++ Model/inference/inference_test.py
@@ -6,14 +6,15 @@ from model_components.auto_fsd import AutoFSD
 def main():
     # Device for inference
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print(f'Using {device} for inference')
+    print(f'Using {device} for inference \n')
             
     # Instantiate model
     model = AutoFSD()
 
     # Dummy Visual Scene Input
     # 7 cameras + 1 map tile - in batch dimension
-    # giving 8 effective visual inputs
+    # giving 8 effective visual inputs assuming batch
+    # size of 1
     visual_tiles = torch.randn(8, 3, 224, 224)
 
     # Dummy Egomotion History Input
@@ -23,17 +24,37 @@ def main():
 
     # Dummy Visual Scene History
     # Length 14 compressed visual feature vector at 10Hz
-    # for 6.4s past horizon giving 896 samples
+    # for 6.4s past horizon giving 64 x 14 samples
     visual_history = torch.randn(896)
     
     # Run inference - returns trajectory and compressed
-    # visual feature vector of the current scene
-    trajectory, compressed_visual_feature_vector = \
+    # visual feature vector of the current scene alongside
+    # a prediction of the future visual state of the scene
+    # in feature space
+    trajectory, compressed_visual_feature_vector, future_visual_features = \
         model(visual_tiles, visual_history, egomotion_history)
 
-    # Print the output tensor shapes
-    print("Trajectory : ", trajectory.shape)
-    print("Visual Feature Vector : ", compressed_visual_feature_vector.shape)
+    # Trajectory Prediction
+    print("---")
+    print("\n")
+    print("Trajectory Prediction: \n")
+    print(trajectory.shape, "\n")
+
+    # Compressed Visual Feature Vector
+    print("---")
+    print("\n")
+    print("Compressed Current Scene Visual Feature Vector: \n")
+    print(compressed_visual_feature_vector.shape, "\n")
+
+    # Future Visual Feature Prediction
+    print("---")
+    print("\n")
+    print("Future Visual Features Prediction: \n")
+    for i in range(0, len(future_visual_features)):
+        print(future_visual_features[i].shape)
+    print("\n")
+
+    print("COMPLETE")
 
 if __name__ == "__main__":
     main()
